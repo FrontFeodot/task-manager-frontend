7350696bffd1b8cb48c0bd99362fbcc5d357b74f
frontfeodot <frontfeodot@gmail.com>
2025-04-08 19:48:33 +0300

    fixing cors issues

--- src/common/api/axios.ts
+++ src/common/api/axios.ts
@@ -3,8 +3,12 @@ import axios from 'axios';
 const BASE_URL = window.origin === 'http://localhost:3000' ? 'http://localhost:4000' : 'https://task-manager-backend-2fp1.onrender.com'
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL, // serverUrl
-  timeout: 10000, // response timeout
+  baseURL: BASE_URL,
+  timeout: 100000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: false,
 });
 
 export default axiosInstance;
